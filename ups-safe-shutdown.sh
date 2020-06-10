790946fca347b9e4d28a4bed44e741b66334a19d
ryougifujino <ryougifujino@gmail.com>
2020-06-10 23:29:53 +0800

    Optimize script

--- ups-safe-shutdown.sh
+++ ups-safe-shutdown.sh
@@ -2,21 +2,21 @@
 
 target_ip=192.168.2.66
 failure_count=0
-shutdown_failure_count_threshold=10
+shutdown_failure_count_threshold=15
 
 while :
 do
-  ping -c 1 -w 5 $target_ip &> /dev/null
+  ping -c 1 $target_ip &> /dev/null
   if [ $? -eq 0 ]; then
     ((failure_count=0))
-    echo 1
   else
     ((failure_count++))
-    echo 2
   fi
-  sleep 20s
+  sleep 10s
   if [ $failure_count -eq $shutdown_failure_count_threshold ]; then
     shutdown -hP now
     break
   fi
 done
+
+exit 0
